4b76321016c4016db4d6d6cfef09730964289967
Cristian Contreras <cristian.contreras@jayway.com>
2016-11-22 16:42:54 +0100

    Updated build task in buildscript

--- Buildscripts/Build.ps1
+++ Buildscripts/Build.ps1
@@ -1,8 +1,10 @@
 Task Default -Depends Build, Push
 
 
+
 Task Build {
     Exec {
+        Push-Location ..\
         nuget restore
         msbuild HelloOctopus.sln /t:build /p:configuration=release,runoctopack=true
     }
